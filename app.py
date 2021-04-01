1698f3ed9a4651f180f5c0b9059060d20f2c39da
ArbindKrPrasad <arbind.kr25801@gmail.com>
2021-04-01 21:49:20 +0530

    GUI Done but does not work when no file is selected

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import keras
 import numpy as np
 from tensorflow.keras.models import model_from_json
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
 
 @app.route('/')
 def index():
@@ -25,18 +26,45 @@ def after():
     loaded_model_json = json_file.read()
     json_file.close()
     model = model_from_json(loaded_model_json)
-    model.load_weights("model_weights1.h5")
+    model.load_weights("model_weights.h5")
 
     ##model = keras.models.load_model("model_weights.h5" )
     prediction = model.predict(image)
 
-    #label_map = np.argmax(prediction)
-    #final_preciction = label_map[prediction]
+    pred = list(prediction[0])
+    anger = int(pred[0]*100)
+    disgust = int(pred[1]*100)
+    fear = int(pred[2]*100)
+    happy = int(pred[3]*100)
+    neutral = int(pred[4]*100)
+    sad = int(pred[5]*100)
+    surprised = int(pred[6]*100)
+    maxNum = max(pred)
+    #for i in range(7):
 
+    indx = pred.index(maxNum)
+    emotion = ""
+    if indx == 0:
+        emotion = "The person is angry ヽ(ಠ_ಠ)ノ"
+    elif indx == 1:
+        emotion = "The person is feeling disgust 「(°ヘ°)"
+    elif indx == 2:
+        emotion = "The person is in fear (⋋▂⋌)"
+    elif indx == 3:
+        emotion = "The person is happy ヽ(•‿•)ノ"
+    elif indx == 4:
+        emotion = "The person is neutral ┐(￣ー￣)┌"
+    elif indx == 5:
+        emotion = "The person is sad (︶︿︶) "
+    elif indx == 6:
+        emotion = "The person is surprised (⊙＿⊙')" 
 
+    color = "#653533"
 
+    return render_template('after.html', data =emotion, angerlvl = anger, disgustlvl = disgust, fearlvl = fear, happylvl = happy, neutrallvl = neutral, sadlvl = sad, surprisedlvl = surprised)
+    
 
-    return render_template('after.html', data =prediction)
+    
 
 
 if __name__ == "__main__":
